fefc99e383311d56cba5bd9bf5c3b7ab544dee70
spacebro-dev <hasnabenali.pro@gmail.com>
2024-11-30 20:56:30 +0200

    ajout image et création scène entrée de jeu

---
+++ Assets/Scripts/DivinationButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DivinationButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private Sprite normalDeck;
+    [SerializeField]
+    private Sprite hoverDeck;
+    [SerializeField]
+    private Image image;
+
+    private bool isMouseHovering;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("that works");
+        image.sprite = hoverDeck;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.sprite = normalDeck;
+    }
+}
