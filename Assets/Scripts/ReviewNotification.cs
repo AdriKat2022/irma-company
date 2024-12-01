00cc92e41238f8b7786ed9cad102fc5e5c0f46c8
Jeremy-afk <jeremylenoir2002@gmail.com>
2024-12-01 12:26:47 +0100

    cleaned the code

--- Assets/Scripts/ReviewNotification.cs
+++ Assets/Scripts/ReviewNotification.cs
@@ -12,27 +12,14 @@ public class ReviewNotification : MonoBehaviour
     [SerializeField] private TextMeshProUGUI contentText;
 
     private int numberOfStars;
-    // A star container to display the rating (made apart from the review notification to implement animations)
-    //[SerializeField] private StarContainer starContainer;
 
-
-    private void Start()
-    {
-        //profilePicture.rectTransform.position = new Vector3(-750f, 350f, 0);
-        //numberOfStarsPicture.rectTransform.position = new Vector3(-300f, 250f, 0);
-    }
-
-    public void InitializeNotification(Sprite profilePicture, string username, int nbOfStars)
+    public void InitializeNotification(Sprite profilePicture, string username, int nbOfStars, string content)
     {
         this.profilePicture.sprite = profilePicture;
         usernameText.text = username;
         numberOfStars = nbOfStars;
-        getNumberOfStars();
-    }
-
-    public void MakeReview(string content)
-    {
         contentText.text = content;
+        getNumberOfStars();
     }
 
     private void getNumberOfStars()
