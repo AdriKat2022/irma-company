7c0801fbb940d47fc8ed6c5995e99b151fd2c360
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-12-02 11:07:37 +0100

    set correct name

--- Assets/MainMenuReturn.cs
+++ Assets/MainMenuReturn.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuReturnTemp : MonoBehaviour
+public class MainMenuReturn : MonoBehaviour
 {
     public string LevelToLoad;
 
